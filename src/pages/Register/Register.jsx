193880bddfaebdbf55d0ac1f0880f2dc6c783cd9
Lenovo <raintruwalker@gmail.com>
2023-02-02 02:31:03 +0300

    Fix Register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -10,9 +10,7 @@ const Register = () => {
                 <div className="wrapper">
                     <h1 className="title">Создать учётную запись</h1>
                     <form className="form">
-                        <input type="text" placeholder="Имя" className="input"/>
-                        <input type="text" placeholder="Фамилия" className="input"/>
-                        <input type="text" placeholder="Отчество" className="input"/>
+                        <input type="text" placeholder="Имя пользователя" className="input"/>
                         <input type="email" placeholder="Эл. почта" className="input"/>
                         <input type="password" placeholder="Пароль" className="input"/>
                         <input type="password" placeholder="Повторите пароль" className="input"/>
