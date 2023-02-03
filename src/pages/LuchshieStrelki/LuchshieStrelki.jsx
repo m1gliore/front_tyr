0e9dd5187980d0fcc0130b9e3373d690cb71e07b
Lenovo <raintruwalker@gmail.com>
2023-02-03 14:15:18 +0300

    Add feature to LuchshieStrelki page

--- src/pages/LuchshieStrelki/LuchshieStrelki.jsx
+++ src/pages/LuchshieStrelki/LuchshieStrelki.jsx
@@ -1,8 +1,48 @@
 import './styles.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from "../../components/Footer/Footer";
+import {useEffect, useState} from "react";
 
 const LuchshieStrelki = () => {
+    const [shooters, setShooters] = useState([])
+
+    useEffect(() => {
+        // (async () => {
+        //     try {
+        //         const response = await axios.get('')
+        //         setImages(response.data)
+        //     } catch (e) {
+        //
+        //     }
+        // })()
+        setShooters(
+            [{
+                "id": 1,
+                "title": "Первая",
+                "url": "https://tir-centr.by/assets/cache_image/lazernoe/ak74/1_250x250_040.jpg",
+                "name": "Алексей",
+                "quantity": 5,
+                "quality": "20%"
+            },
+                {
+                    "id": 2,
+                    "title": "Вторая",
+                    "url": "https://tir-centr.by/assets/cache_image/lazernoe/ak74/1_250x250_040.jpg",
+                    "name": "Антон",
+                    "quantity": 7,
+                    "quality": "40%"
+                },
+                {
+                    "id": 3,
+                    "title": "Третья",
+                    "url": "https://tir-centr.by/assets/cache_image/lazernoe/ak74/1_250x250_040.jpg",
+                    "name": "Андрей",
+                    "quantity": 9,
+                    "quality": "60%"
+                }])
+    }, [])
+
+
     return (
         <>
             <Navbar/>
@@ -16,42 +56,20 @@ const LuchshieStrelki = () => {
                     </nav>
                     <h2>Лучшие стрелки</h2>
                     <div className="row">
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card">
-                                <img className="card-img-top mx-auto d-block"
-                                     src="https://tir-centr.by/assets/cache_image/lazernoe/ak74/1_250x250_040.jpg"
-                                     alt="Лазертаг с автоматом Калашникова - лазертаг с АК"/>
-                                <div className="card-body">
-                                    <p className="card-title card-desc">Алексей</p>
-                                    <p className="card-desc">Количество выстрелов: 5</p>
-                                    <p className="card-desc">Процент попаданий: 20%</p>
+                        {shooters.map((shooter) =>
+                            <div className="col-lg-4 col-md-6" key={shooter.id}>
+                                <div className="card">
+                                    <img className="card-img-top mx-auto d-block"
+                                         src={shooter.url}
+                                         alt={shooter.title}/>
+                                    <div className="card-body">
+                                        <p className="card-title card-desc">{shooter.name}</p>
+                                        <p className="card-desc">Количество выстрелов: {shooter.quantity}</p>
+                                        <p className="card-desc">Процент попаданий: {shooter.quality}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card">
-                                <img className="card-img-top mx-auto d-block"
-                                     src="https://tir-centr.by/assets/cache_image/lazernoe/ak74/1_250x250_040.jpg"
-                                     alt="Лазертаг с автоматом Калашникова - лазертаг с АК"/>
-                                <div className="card-body">
-                                    <p className="card-title card-desc">Антон</p>
-                                    <p className="card-desc">Количество выстрелов: 7</p>
-                                    <p className="card-desc">Процент попаданий: 40%</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card">
-                                <img className="card-img-top mx-auto d-block"
-                                     src="https://tir-centr.by/assets/cache_image/lazernoe/ak74/1_250x250_040.jpg"
-                                     alt="Лазертаг с автоматом Калашникова - лазертаг с АК"/>
-                                <div className="card-body">
-                                    <p className="card-title card-desc">Андрей</p>
-                                    <p className="card-desc">Количество выстрелов: 9</p>
-                                    <p className="card-desc">Процент попаданий: 60%</p>
-                                </div>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </section>
