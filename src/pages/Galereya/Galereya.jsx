cfc448f2da001f24435352b93bd174ce0b2ea003
Lenovo <raintruwalker@gmail.com>
2023-02-03 22:36:15 +0300

    Add Backend bound to Galereya page

--- src/pages/Galereya/Galereya.jsx
+++ src/pages/Galereya/Galereya.jsx
@@ -3,7 +3,7 @@ import lightbox from 'lightbox2/dist/js/lightbox-plus-jquery.js'
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from "../../components/Footer/Footer";
 import {useEffect, useState} from "react";
-// import axios from "axios";
+import axios from "axios";
 
 const Galereya = () => {
     lightbox.option({
@@ -14,46 +14,19 @@ const Galereya = () => {
     const [images, setImages] = useState([])
 
     useEffect(() => {
-        // (async () => {
-        //     try {
-        //         const response = await axios.get('')
-        //         setImages(response.data)
-        //     } catch (e) {
-        //
-        //     }
-        // })()
-        setImages(
-            [{
-                "id": 1,
-                "title": "Первая",
-                "url": "https://tir-centr.by/assets/images/gallery/015.jpg"
-            },
-                {
-                    "id": 2,
-                    "title": "Вторая",
-                    "url": "https://tir-centr.by/assets/images/gallery/023.jpg"
-                },
-                {
-                    "id": 3,
-                    "title": "Третья",
-                    "url": "https://tir-centr.by/assets/images/gallery/024.jpg"
-                },
-                {
-                    "id": 4,
-                    "title": "Четверая",
-                    "url": "https://tir-centr.by/assets/images/gallery/033.jpg"
-                },
-                {
-                    "id": 5,
-                    "title": "Пятая",
-                    "url": "https://tir-centr.by/assets/images/gallery/034.jpg"
-                },
-                {
-                    "id": 6,
-                    "title": "Шестая",
-                    "url": "https://tir-centr.by/assets/images/gallery/035.jpg"
-                }])
+        (async () => {
+            try {
+                const response = await axios.get('http://localhost:8040/api/homePage/gallery')
+                setImages(response.data.imageResponseSet)
+            } catch (e) {
+
+            }
+        })()
     }, [])
+
+    images.sort((a, b) => {
+        return a.idImage - b.idImage
+    })
 
     return (
         <>
@@ -61,8 +34,11 @@ const Galereya = () => {
             <main role="main">
                 <div className="galleryContainer">
                     {images.map((image) =>
-                        <a key={image.id} href={image.url} data-lightbox="images" data-title={image.title}>
-                            <img src={image.url} alt={image.id}/>
+                        <a key={image.idImage} href={"data:image/" + image.url.split('.')[1] + ";base64," + image.file}
+                           data-lightbox="images"
+                           data-title={image.url}>
+                            <img src={"data:image/" + image.url.split('.')[1] + ";base64," + image.file}
+                                 alt={image.idImage}/>
                         </a>
                     )}
                 </div>
