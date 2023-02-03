d2ec2c43d4822babb964087ad9357938c9d83ea0
Lenovo <raintruwalker@gmail.com>
2023-02-03 21:09:52 +0300

    Add feature to Czenyi page

--- src/pages/Czenyi/Czenyi.jsx
+++ src/pages/Czenyi/Czenyi.jsx
@@ -1,8 +1,54 @@
 import './styles.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from "../../components/Footer/Footer";
+import {useEffect, useState} from "react";
 
 const Czenyi = () => {
+    const [prices, setPrices] = useState([])
+
+    useEffect(() => {
+        // (async () => {
+        //     try {
+        //         const response = await axios.get('')
+        //         setImages(response.data)
+        //     } catch (e) {
+        //
+        //     }
+        // })()
+        setPrices(
+            [{
+                "id": 1,
+                "title": "Пневматический тир (10 выстрелов)",
+                "quantity": 4
+            },
+                {
+                    "id": 2,
+                    "title": "Страйкбольный тир (20 выстрелов)",
+                    "quantity": 6
+                },
+                {
+                    "id": 3,
+                    "title": "Лазерный тир (15 выстрелов)",
+                    "quantity": 2
+                },
+                {
+                    "id": 4,
+                    "title": "Пристрелка страйкбольного привода",
+                    "quantity": 9
+                },
+                {
+                    "id": 5,
+                    "title": "Пристрелка пневматики",
+                    "quantity": 9
+                },
+                {
+                    "id": 6,
+                    "title": "Аренда тира (со своим оружием), 30 минут",
+                    "quantity": 25
+                }
+            ])
+    }, [])
+
     return (
         <>
             <Navbar/>
@@ -28,93 +74,12 @@ const Czenyi = () => {
                             </div>
                             <table className="table table-sm price-table">
                                 <tbody>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/oruzhie-v-tire/pnevmatika">
-                                    <td>Пневматический тир (10 выстрелов)</td>
-                                    <td className="price-cell-price">от&nbsp;4&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/oruzhie-v-tire/strajkbol">
-                                    <td>Страйкбольный тир (20 выстрелов)</td>
-                                    <td className="price-cell-price">от&nbsp;6&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/oruzhie-v-tire/lazernoe">
-                                    <td>Лазерный тир (15 выстрелов)</td>
-                                    <td className="price-cell-price">от&nbsp;2&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/osnovnyie-uslugi-tira/pristrelka-strajkbolnogo-privoda">
-                                    <td>Пристрелка страйкбольного привода</td>
-                                    <td className="price-cell-price">от&nbsp;9&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/osnovnyie-uslugi-tira/pristrelka-pnevmatiki">
-                                    <td>Пристрелка пневматики</td>
-                                    <td className="price-cell-price">от&nbsp;9&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/osnovnyie-uslugi-tira/arenda-tira-v-minske">
-                                    <td>Аренда тира (со своим оружием), 30 минут</td>
-                                    <td className="price-cell-price">от&nbsp;25&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/osnovnyie-uslugi-tira/izmerenie-dulnoj-energii">
-                                    <td>Измерение дульной энергии</td>
-                                    <td className="price-cell-price">от&nbsp;10&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/obuchenie-i-kursyi-strelbyi">
-                                    <td>Обучение и курсы стрельбы</td>
-                                    <td className="price-cell-price">от&nbsp;90&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/obuchenie-i-kursyi-strelbyi/sorevnovaniya-po-strelbe">
-                                    <td>Соревнования по стрельбе (чел.)</td>
-                                    <td className="price-cell-price">от&nbsp;10&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/uslugi/yuridicheskim-liczam">
-                                    <td>Юридическим лицам (корпоративы, тимбилдинг), на человека</td>
-                                    <td className="price-cell-price">от&nbsp;30&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/razvlecheniya-dlya-vsej-semi">
-                                    <td>Семейный досуг и ДР, на человека</td>
-                                    <td className="price-cell-price">от&nbsp;30&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/uslugi/podarochnyie-sertifikatyi">
-                                    <td>Подарочные сертификаты</td>
-                                    <td className="price-cell-price">от&nbsp;30&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/uslugi/poznanie-i-razvitie">
-                                    <td>Экскурсии и лекции, музей оружия (от 2 ч., до 5 чел.)</td>
-                                    <td className="price-cell-price">от&nbsp;40&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/uslugi/standartnyie-paketyi">
-                                    <td>Стандартные пакеты услуг (при затруднениях с выбором, на человека)</td>
-                                    <td className="price-cell-price">от&nbsp;10&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/osnovnyie-uslugi-tira/vyiezdnoj-tir-–-minsk-i-minskaya-oblast">
-                                    <td>Выездной тир</td>
-                                    <td className="price-cell-price">от&nbsp;40&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/osnovnyie-uslugi-tira/shimming-i-remont">
-                                    <td>Шимминг и ремонт приводов</td>
-                                    <td className="price-cell-price">от&nbsp;55&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/obuchenie-i-kursyi-strelbyi/sekcziya-strelbyi-dlya-detej">
-                                    <td>Детская секция стрельбы (8 занятий)</td>
-                                    <td className="price-cell-price">от&nbsp;240&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href"
-                                    tr-href="https://tir-centr.by/uslugi/yuridicheskim-liczam/podgotovka-k-sorevnovaniyam-po-strelbe,-trenirovka-–-dlya-yuridicheskix-licz">
-                                    <td>Юридическим лицам - подготовка к соревнованиям по стрельбе</td>
-                                    <td className="price-cell-price">от&nbsp;190&nbsp;руб.</td>
-                                </tr>
-                                <tr className="tr-href" tr-href="https://tir-centr.by/oruzhie-v-tire/signalnoe-oruzhie">
-                                    <td>Сигнальное оружие (1 выстрел)</td>
-                                    <td className="price-cell-price">от&nbsp;3&nbsp;руб.</td>
-                                </tr>
+                                {prices.map((price) =>
+                                    <tr className="tr-href">
+                                        <td>{price.title}</td>
+                                        <td className="price-cell-price">от&nbsp;{price.quantity}&nbsp;руб.</td>
+                                    </tr>
+                                )}
                                 </tbody>
                             </table>
                         </div>
