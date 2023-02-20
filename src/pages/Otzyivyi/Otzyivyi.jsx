d7b4a5793f23fd1c3e5719cbaa8c5a9972a17cc1
Lenovo <raintruwalker@gmail.com>
2023-02-20 22:19:18 +0300

    Fix Otzyivyi page

--- src/pages/Otzyivyi/Otzyivyi.jsx
+++ src/pages/Otzyivyi/Otzyivyi.jsx
@@ -4,41 +4,48 @@ import Footer from "../../components/Footer/Footer";
 import {useEffect, useState} from "react";
 import Modal from "../../components/Modal/Modal";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+import {faBan, faCheckCircle} from "@fortawesome/free-solid-svg-icons";
 import {useNavigate} from "react-router-dom";
 import {userRequest} from "../../requestMethods";
 import {isAdmin} from "../../myLibrary";
 
 const Otzyivyi = () => {
+    const [allReplies, setAllReplies] = useState([])
     const [replies, setReplies] = useState([])
+    const [cardNumber, setCardNumber] = useState(null)
     const [modalAcceptActive, setModalAcceptActive] = useState(false)
-    const [imageUrl, setImageUrl] = useState("")
+    const [modalRejectActive, setModalRejectActive] = useState(false)
     const admin = isAdmin()
     const navigate = useNavigate()
 
     useEffect(() => {
         (async () => {
             try {
-                const response = await userRequest.get(`http://localhost:8040/api/redact1/getAllReview`)
-                setReplies(response.data)
-                console.log(response.data)
+                const responseAdmin = await userRequest.get(`http://localhost:8040/api/redact1/getAllReview`)
+                const responseClient = await userRequest.get(`http://localhost:8040/api/homePage/getAllReviewsByHomePage`)
+                setAllReplies(responseAdmin.data)
+                setReplies(responseClient.data)
             } catch (e) {
                 console.log(e)
             }
         })()
-    }, [])
+    }, [allReplies])
+
+    allReplies.sort((a, b) => {
+        return a.idReview - b.idReview
+    })
 
     replies.sort((a, b) => {
         return a.idReview - b.idReview
     })
 
     const handleSubmitRedact = async (event) => {
+        console.log(event)
         event.preventDefault()
         try {
-            const id = event.target.id.value
-            const idReview = replies[id].idReview
+            const idReview = cardNumber
             const reviewStatus = event.target.status.value
-            const username = replies[id].username
+            const username = allReplies.find(item => item.idReview === idReview).username
             const myJson = {
                 idReview,
                 reviewStatus,
@@ -51,65 +58,72 @@ const Otzyivyi = () => {
         }
     }
 
-    const handleSelectRedact = (event) => {
-        event.preventDefault()
-        console.log(event.target.value)
-        const selectedImage = replies[event.target.value]
-        const selectedImageUrl = selectedImage.idReview
-        setImageUrl(selectedImageUrl)
-    }
-
     return (
         <>
             <Navbar/>
-            {admin &&
-                <FontAwesomeIcon className="action fa-2x" icon={faCheckCircle} onClick={() => {
-                    setModalAcceptActive(true)
-                    setImageUrl("")
-                }}/>}
             <section className="shooters-list-str bg-white">
                 <div className="container">
-                    <nav aria-label="breadcrumb">
-                        <ol className="breadcrumb">
-                            <li className="breadcrumb-item"><a href="/">Главная</a></li>
-                            <li className="breadcrumb-item active">Отзывы</li>
-                        </ol>
-                    </nav>
                     <h2>Отзывы</h2>
                     <div className="row">
                         {replies.map((reply) =>
                             <div className="col-lg-4 col-md-6" key={reply.idReview}>
                                 <div className="cardStr">
                                     <div className="card-body-str">
-                                        <p className="card-desc-str">Пользователь:{reply.username}</p>
-                                        <p className="card-desc-str">{reply.dateRegistration}</p>
-                                        <p className="card-desc-str">Отзыв:{reply.reviewInfo}</p>
+                                        <p className="card-desc-str">Пользователь: {reply.username}</p>
+                                        <p className="card-desc-str">Дата: {reply.dateRegistration}</p>
+                                        <p className="card-desc-str">Отзыв:<br/><br/>{reply.reviewInfo}</p>
                                     </div>
                                 </div>
                             </div>
                         )}
                     </div>
+                    {admin && <>
+                        {allReplies.length !== 0 && <h2 style={{marginTop: "5vh"}}>Неподтверждённые отзывы</h2>}
+                        <div className="row">
+                            {allReplies.map((reply) =>
+                                <div className="col-lg-4 col-md-6" key={reply.idReview}>
+                                    <div className="cardStr">
+                                        <div className="card-body-str">
+                                            <p className="card-desc-str">Пользователь: {reply.username}</p>
+                                            <p className="card-desc-str">Дата: {reply.dateRegistration}</p>
+                                            <p className="card-desc-str">Отзыв:<br/><br/>{reply.reviewInfo}</p>
+                                        </div>
+                                        {admin && <div style={{display: "flex"}}>
+                                            <FontAwesomeIcon className="action fa-2x" icon={faCheckCircle}
+                                                             onClick={() => {
+                                                                 setModalAcceptActive(true)
+                                                                 setCardNumber(reply.idReview)
+                                                             }}/>
+                                            <FontAwesomeIcon className="action fa-2x" icon={faBan}
+                                                             onClick={() => {
+                                                                 setModalRejectActive(true)
+                                                                 setCardNumber(reply.idReview)
+                                                             }}/>
+                                        </div>}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </>}
                 </div>
             </section>
             <Footer/>
             {admin && <>
                 <Modal active={modalAcceptActive} setActive={setModalAcceptActive}>
-                    <h1>Изменить статус отзыва</h1>
+                    <h1>Принять отзыв</h1>
                     <form className="modalAdd" onSubmit={handleSubmitRedact}>
-                        <div className="leftContainer">{imageUrl}</div>
+                        <input type="text" name="status" value="CONFIRMED" style={{display: "none"}}/>
                         <div className="rightContainer">
-                            <select required className="inputAdd" name="id" onChange={handleSelectRedact}>
-                                <option selected disabled value="">Выберите один из вариантов</option>
-                                {replies.map((item) =>
-                                    <option key={item.idReview}
-                                            value={replies.indexOf(item)}>{replies.indexOf(item) + 1}</option>)}
-                            </select>
-                            <select required className="inputAdd" name="status">
-                                <option selected disabled value="">Выберите статус</option>
-                                <option value="CONFIRMED">Принять</option>
-                                <option value="REJECTED">Отклонить</option>
-                            </select>
-                            <button className="buttonAdd">Изменить</button>
+                            <button className="buttonAdd">Принять</button>
+                        </div>
+                    </form>
+                </Modal>
+                <Modal active={modalRejectActive} setActive={setModalRejectActive}>
+                    <h1>Отклонить отзыв</h1>
+                    <form className="modalAdd" onSubmit={handleSubmitRedact}>
+                        <input type="text" name="status" value="REJECTED" style={{display: "none"}}/>
+                        <div className="rightContainer">
+                            <button className="buttonAdd">Отклонить</button>
                         </div>
                     </form>
                 </Modal>
